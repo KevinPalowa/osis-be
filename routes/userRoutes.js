6f7a62b18f8ff23b81357eb94f69d902f07d6b0e
Kevin Palowa <kevinpalowa@gmail.com>
2024-07-22 00:27:43 +0700

    add jwt middleware in create user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,12 @@ const upload = require("../middleware/multerConfig");
 
 const router = express.Router();
 
-router.post("/users", upload.single("photo"), userController.createUser);
+router.post(
+  "/users",
+  authenticateJWT,
+  upload.single("photo"),
+  userController.createUser
+);
 router.post("/login", userController.login);
 router.get("/users/all", authenticateJWT, userController.getAllUsers);
 router.get("/users/:id", authenticateJWT, userController.getUser);
